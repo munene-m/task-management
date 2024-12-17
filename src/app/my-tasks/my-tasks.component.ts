c5bcaebd9570c84bcd8393b3e3290c6e8a565545
munene-m <munenenjue18@gmail.com>
2024-12-17 13:31:39 +0300

    chore: load tasks on page init load

--- src/app/my-tasks/my-tasks.component.ts
+++ src/app/my-tasks/my-tasks.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { IndexedDbService, Task } from '../services/dbService';
 
 @Component({
   selector: 'app-my-tasks',
   templateUrl: './my-tasks.component.html',
   styleUrls: ['./my-tasks.component.css']
 })
-export class MyTasksComponent {
+export class MyTasksComponent implements OnInit {
+  searchTerm = '';
+  private tasksSubject = new BehaviorSubject<Task[]>([]);
+  tasks$ = this.tasksSubject.asObservable();
+  
+  constructor(private indexedDbService: IndexedDbService) {}
+  
+  ngOnInit() {
+    this.loadTasks();
+  }
 
+  async deleteTask(id: number){
+  }
+
+  async loadTasks() {
+    const tasks = await this.indexedDbService.getTasks();
+    this.tasksSubject.next(tasks);
+  }
 }
